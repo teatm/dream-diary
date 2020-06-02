49a8a1864fb02ae9b1d136009ea3db84254e0131
Albert <albert@caulis.jp>
2020-06-02 22:58:18 +0900

    Move diary- and item-related components to diary and item folders

--- dream-diary-client/src/app/app-routing.module.ts
+++ dream-diary-client/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DiaryListComponent } from './components/diary-list/diary-list.component';
-import { DiaryDetailComponent } from './components/diary-detail/diary-detail.component';
-import { DiaryCreateComponent } from './components/diary-create/diary-create.component';
-import { ItemListComponent } from './components/item-list/item-list.component';
-import { ItemDetailComponent } from './components/item-detail/item-detail.component';
-import { ItemCreateComponent } from './components/item-create/item-create.component';
+import { DiaryListComponent } from './components/diary/diary-list/diary-list.component';
+import { DiaryDetailComponent } from './components/diary/diary-detail/diary-detail.component';
+import { DiaryCreateComponent } from './components/diary/diary-create/diary-create.component';
+import { ItemListComponent } from './components/item/item-list/item-list.component';
+import { ItemDetailComponent } from './components/item/item-detail/item-detail.component';
+import { ItemCreateComponent } from './components/item/item-create/item-create.component';
 
 
 const routes: Routes = [
--- dream-diary-client/src/app/components/diary/diary-create/diary-create.component.ts
+++ dream-diary-client/src/app/components/diary/diary-create/diary-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DiaryService } from '../../services/diary.service';
+import { DiaryService } from '../../../services/diary.service';
 import { Router } from '@angular/router'
 
 @Component({
